138ce4f2df0a935d6628e913414c4c75eca98cfe
Anton Berger <aweo.wew@mail.ru>
2023-11-10 19:56:41 +0300

    add events from websocket

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { ENV } from '@app/env';
-import { OnDestroy , Component } from '@angular/core';
+import { OnDestroy, Component } from '@angular/core';
 import { Subject } from 'rxjs';
 import { TokenService } from "../../core/services/token.service";
 import { WebsocketService } from "../../core/services/websocket/websocket.service";
@@ -10,44 +10,25 @@ const { ws } = ENV;
 })
 export class HomePageComponent implements OnDestroy {
 	private destroy$ = new Subject<void>();
+	public events!: any;
 
 	constructor(private wsService: WebsocketService, private tokenService: TokenService) {
+
 		tokenService.receive().subscribe({
-			next: ((token) => {
+			next: ((token: string) => {
 				const url = `${ws}?token=${token}`;
 				wsService.connect(url);
 			})
 		});
-		// Ниже примеры отправки
-
-		// Если появиласб очередь использовать pipe first() или аналоги для автоматической отписки
-
-		// Описать типы для body
-		// так отправлять запрос на вебсокет
-		// setTimeout(() => {
-		// 	wsService.send({
-		// 		method: "new",
-		// 		body: {
-		// 			min: 600,
-		// 			time_start: 1694244600,
-		// 			lat:59.80228462,
-		// 			lon:30.37665965,
-		// 			radius:1000
-		// 		}
-		// 	});
-		// }, 1000);
-
-
-		// Получение результата сообщения
-		// setTimeout(() => {
-		// 	wsService.on().subscribe({
-		// 		next:((result) => {
-		// 			console.log(result);
-
-		// 		})
-		// 	});
-		// }, 1000);
 	}
+
+	public getEvents(event: any) {
+		console.log(event)
+		this.events = event.events;
+	}
+
+
+
 
 	public ngOnDestroy(): void {
 		this.destroy$.next();
